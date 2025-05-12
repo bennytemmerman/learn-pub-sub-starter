b640fe701397a1137c30761772fd4a8b0df4ec91
bennytemmerman <arcainwolf91@gmail.com>
2025-05-12 20:53:45 +0200

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -24,6 +24,48 @@ func main() {
 		log.Fatalf("could not create channel: %v", err)
 	}
 
+	// Declare the peril_topic exchange (topic exchange)
+	err = publishCh.ExchangeDeclare(
+		routing.ExchangePerilTopic, // name of the exchange
+		"topic",                    // type
+		true,                       // durable
+		false,                      // auto-deleted
+		false,                      // internal
+		false,                      // no-wait
+		nil,                        // arguments
+	)
+	if err != nil {
+		log.Fatalf("could not declare exchange: %v", err)
+	}
+
+	// Declare the durable queue game_logs
+	queue, err := publishCh.QueueDeclare(
+		"game_logs", // name
+		true,        // durable
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	if err != nil {
+		log.Fatalf("could not declare queue: %v", err)
+	}
+
+	// Bind the queue to the topic exchange with routing key game_logs.*
+	err = publishCh.QueueBind(
+		queue.Name,                 // name of the queue
+		routing.GameLogSlug+".*",  // routing key (e.g., game_logs.*)
+		routing.ExchangePerilTopic, // exchange
+		false,                     // no-wait
+		nil,                       // arguments
+	)
+	if err != nil {
+		log.Fatalf("could not bind queue: %v", err)
+	}
+
+	fmt.Println("Queue 'game_logs' bound to exchange 'peril_topic' with routing key 'game_logs.*'")
+
+
 	gamelogic.PrintServerHelp()
 
 	for {
