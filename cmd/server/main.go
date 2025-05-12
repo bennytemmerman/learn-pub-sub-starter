5d8e48f7d3720db9e88cd438e5886dddc1a40b29
bennytemmerman <arcainwolf91@gmail.com>
2025-05-12 21:15:18 +0200

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/bennytemmerman/learn-pub-sub-starter/internal/gamelogic"
-	"github.com/bennytemmerman/learn-pub-sub-starter/internal/pubsub"
-	"github.com/bennytemmerman/learn-pub-sub-starter/internal/routing"
-	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -24,47 +25,17 @@ func main() {
 		log.Fatalf("could not create channel: %v", err)
 	}
 
-	// Declare the peril_topic exchange (topic exchange)
-	err = publishCh.ExchangeDeclare(
-		routing.ExchangePerilTopic, // name of the exchange
-		"topic",                    // type
-		true,                       // durable
-		false,                      // auto-deleted
-		false,                      // internal
-		false,                      // no-wait
-		nil,                        // arguments
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug+".*",
+		pubsub.SimpleQueueDurable,
 	)
 	if err != nil {
-		log.Fatalf("could not declare exchange: %v", err)
+		log.Fatalf("could not subscribe to pause: %v", err)
 	}
-
-	// Declare the durable queue game_logs
-	queue, err := publishCh.QueueDeclare(
-		"game_logs", // name
-		true,        // durable
-		false,       // delete when unused
-		false,       // exclusive
-		false,       // no-wait
-		nil,         // arguments
-	)
-	if err != nil {
-		log.Fatalf("could not declare queue: %v", err)
-	}
-
-	// Bind the queue to the topic exchange with routing key game_logs.*
-	err = publishCh.QueueBind(
-		queue.Name,                 // name of the queue
-		routing.GameLogSlug+".*",  // routing key (e.g., game_logs.*)
-		routing.ExchangePerilTopic, // exchange
-		false,                     // no-wait
-		nil,                       // arguments
-	)
-	if err != nil {
-		log.Fatalf("could not bind queue: %v", err)
-	}
-
-	fmt.Println("Queue 'game_logs' bound to exchange 'peril_topic' with routing key 'game_logs.*'")
-
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
 
 	gamelogic.PrintServerHelp()
 
